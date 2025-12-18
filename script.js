ac546d3ba7389b92e5c1e58007ecb1da12fbba1d
Lucia Avilla <avilla.lucia@gmail.com>
2025-12-18 16:57:49 -0300

    Captura do evento e objeto de nova despesa criado

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Seleciona os elementos do form
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
 // Captura o evento de input para formatar o valor.
 amount.oninput = () => {
@@ -16,8 +19,24 @@ amount.oninput = () => {
 }
 
 function formatCurrencyBRL(value) {
+    // Formata o valor para o padrão brasileiro
     return value.toLocaleString("pt-BR", {
         style: "currency",
         currency: "BRL",
     })
+}
+
+// Captura o evento de submit do form para obter os valores
+form.onsubmit = (event) => {
+    // Previne o comportamento padrão de fazer o reload da página
+    event.preventDefault()
+    // cria um objeto com os detalhes da nova despesa
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
 }
