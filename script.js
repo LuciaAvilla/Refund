24ce7fd7fae6afdbaf364d1a9592787af26d11dc
Lucia Avilla <avilla.lucia@gmail.com>
2026-01-06 11:30:44 -0300

    Implementando o ícone de exclusão

--- script.js
+++ script.js
@@ -78,8 +78,14 @@ function expenseAdd(newExpense) {
         expenseAmount.classList.add("expense-amount")
         expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`
 
+        // criando o ícone de exclusão
+        const removeIcon = document.createElement("img")
+        removeIcon.classList.add("remove-icon")
+        removeIcon.setAttribute("src", "img/remove.svg")
+        removeIcon.setAttribute("alt", "remover")
+
         // Adiciona informações no item
-        expenseItem.append(expenseIcon, expenseInfo, expenseAmount)
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
 
         // Adiciona o item na lista
         expenseList.append(expenseItem)
