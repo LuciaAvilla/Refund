7db57301a7cb801531e2e37c2260e78efd745d25
Lucia Avilla <avilla.lucia@gmail.com>
2026-01-06 15:58:24 -0300

    Exibindo a quantidade de itens da lista

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const category = document.getElementById("category")
 
 // Seleciona os elementos da lista
 const expenseList = document.querySelector("ul")
+const expensesQuantity = document.querySelector(" aside header p span")
 
 // Captura o evento de input para formatar o valor.
 amount.oninput = () => {
@@ -46,6 +47,7 @@ form.onsubmit = (event) => {
     expenseAdd(newExpense)
 }
 
+// Adiciona um novo item na lista
 function expenseAdd(newExpense) {
 
     try {
@@ -90,10 +92,28 @@ function expenseAdd(newExpense) {
         // Adiciona o item na lista
         expenseList.append(expenseItem)
 
+        //Atualiza os totais
+        updatesTotals()
+
     } catch (error) {
         alert("Não foi possível atualizar a lista de despesas!")
         console.log(error)
     }
 
 
+}
+
+// atualiza os totais
+function updatesTotals() {
+    try {
+        // Recupera todos os itens (li) da lista (ul)
+        const items = expenseList.children
+
+        // Atualiza a quantidade de itens da lista 
+        expensesQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+
+    } catch (error) {
+        console.log(error)
+        alert("Não foi possível atualizar os totais")
+    }
 }
